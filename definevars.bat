7ee69dc6a0e829110237e5a4a2182fc0a440549f
Naohide Sano <namachapanda2005@gmail.com>
2022-10-03 15:59:52 +0900

    preparation to merge

--- definevars.bat
+++
@@ -1,3 +0,0 @@
-set LIBDIR=%~dp0targets\application\lib
-
-set DMGX_CLASSPATH=%LIBDIR%\dmgextractor.jar
--- definevars.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export LIBDIR="targets/application/lib"
-
-export DMGX_CLASSPATH="$LIBDIR/dmgextractor.jar"
-echo "$DMGX_CLASSPATH"
